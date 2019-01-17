c4c535a2ad6f1529b6ff2da6324d0753c85027d0
Aiden Nibali <dismaldenizen@gmail.com>
2019-01-18 09:01:34 +1100

    Improve RGB conversion code

--- tests/test_tvl.py
+++ tests/test_tvl.py
@@ -33,6 +33,23 @@ class TorchMemManager(tvlnv.MemManager):
         return ptr
 
 
+def nv12_to_rgb(planar_yuv, h, w):
+    rgb = torch.empty([3, h, w], dtype=torch.float32, device=planar_yuv.device)
+    # Memory reuse trick
+    v, _, u = rgb
+    # Extract luma channel
+    y = planar_yuv[:w*h].view(h, w).float()
+    # Extract and upsample chroma channels
+    u.copy_(planar_yuv[w*h::2].view(h//2, 1, w//2, 1).expand(h//2, 2, w//2, 2).contiguous().view(h, w))
+    v.copy_(planar_yuv[w*h+1::2].view(h//2, 1, w//2, 1).expand(h//2, 2, w//2, 2).contiguous().view(h, w))
+    # YUV [0, 255] to RGB [0, 1]
+    y.mul_(4.566207e-3)
+    torch.add(u, 2.075161, v, out=rgb[1]).mul_(-1.536320e-3).add_(y).add_(0.5316706) # Green
+    v.mul_(6.258931e-3).add_(y).add_(-0.8742) # Red
+    u.mul_(7.910723e-3).add_(y).add_(-1.0856313) # Blue
+    return rgb.clamp_(0, 1)
+
+
 def test_torch_mem_manager():
     mm = TorchMemManager(torch.device('cuda:1'))
     mm.__disown__()
@@ -44,24 +61,18 @@ def test_torch_mem_manager():
     fr = tvlnv.TvlnvFrameReader(mm, VIDEO_FILENAME, mm.device.index, resize_dim=dim)
 
     fr.seek(120.0)
-    start = time()
-    frame_ptr = int(fr.read_frame())
+    planar_yuv = mm.tensors[int(fr.read_frame())]
 
     w = fr.get_width()
     h = fr.get_height()
 
-    b = mm.tensors[frame_ptr]
-
-    # Format is NV12 (I think)
-    yuv = b.cpu()
-    y = yuv[:w*h].view(h, w).numpy()
-    uv = yuv[w*h:]
-    u = uv[::2].view(h//2, w//2).numpy().repeat(2, axis=0).repeat(2, axis=1)
-    v = uv[1::2].view(h//2, w//2).numpy().repeat(2, axis=0).repeat(2, axis=1)
-    img = PIL.Image.fromarray(np.stack([y, u, v], axis=-1), 'YCbCr')
-    img.show()
-
+    start = time()
+    rgb = nv12_to_rgb(planar_yuv, h, w)
     print(time() - start)
+
+    rgb_bytes = (rgb * 255).round_().byte().cpu()
+    img = PIL.Image.fromarray(rgb_bytes.permute(1, 2, 0).numpy(), 'RGB')
+    img.show()
 
 
 
