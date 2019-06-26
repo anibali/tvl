79d071f3669bd8125a144b31d84669cacbb50d04
Matt Oliver <protogonoi@gmail.com>
2019-06-26 21:54:45 +1000

    FFFR: Update memory allocation with correct size and strides.

--- tvl_backends/tvl-backends-fffr/src/tvl_backends/fffr/memory.py
+++ tvl_backends/tvl-backends-fffr/src/tvl_backends/fffr/memory.py
@@ -7,6 +7,9 @@ def _dtype_bytes(dtype):
     assert info.bits % 8 == 0
     return info.bits // 8
 
+def _align_to(value, alignment):
+    return (value + (alignment - 1)) & ~(alignment - 1)
+
 
 class TorchImageAllocator(pyfffr.ImageAllocator):
     def __init__(self, device, dtype):
@@ -25,19 +28,21 @@ class TorchImageAllocator(pyfffr.ImageAllocator):
         align_bytes = 32
         elem_size = _dtype_bytes(self.dtype)
         assert align_bytes % elem_size == 0
+        line_elems = line_size // elem_size
+        align_elems = align_bytes // elem_size
 
-        n_padded_elems = 3 * (height * line_size + align_bytes // elem_size)
+        n_padded_elems = 3 * (height * line_elems + align_elems)
         storage = torch.empty(n_padded_elems, device=self.device, dtype=self.dtype).storage()
         ptr = storage.data_ptr()
         assert ptr % elem_size == 0
-        aligned_ptr = (((ptr - 1) // align_bytes) * align_bytes + (align_bytes - ptr % align_bytes))
+        aligned_ptr = _align_to(ptr, align_bytes)
         storage_offset = (aligned_ptr - ptr) // elem_size
-        plane_stride_bytes = (((height * line_size * elem_size - 1) // align_bytes) + 1) * align_bytes
+        plane_stride_bytes = _align_to(height * line_size, align_bytes)
         plane_stride = plane_stride_bytes // elem_size
 
         tensor = torch.empty((0,), device=self.device, dtype=self.dtype)
         tensor.set_(storage, storage_offset=storage_offset, size=(3, height, width),
-                    stride=(plane_stride, line_size, 1))
+                    stride=(plane_stride, line_elems, 1))
         address = tensor.data_ptr()
 
         self.tensors[address] = tensor
