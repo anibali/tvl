e24ed5636fe45fc44a2bfa5c7238850d37d7aad3
Aiden Nibali <dismaldenizen@gmail.com>
2020-02-19 14:15:42 +1100

    Fix cleaning of MANIFEST file

--- tvl_backends/tvl-backends-fffr/setup.py
+++ tvl_backends/tvl-backends-fffr/setup.py
@@ -1,8 +1,26 @@
+import os
+from distutils import log
 from pathlib import Path
 
 from skbuild import setup
+from skbuild.command.clean import clean
 
 version = Path(__file__).absolute().parent.joinpath('VERSION').read_text('utf-8').strip()
+
+
+class CustomClean(clean):
+    FILES_TO_REMOVE = ['MANIFEST']
+
+    def run(self):
+        super().run()
+        for filename in self.FILES_TO_REMOVE:
+            if os.path.isfile(filename):
+                log.info("removing '%s'", filename)
+            else:
+                log.debug("'%s' does not exist -- can't clean it", filename)
+            if not self.dry_run and os.path.exists(filename):
+                os.remove(filename)
+
 
 setup(
     name='tvl-backends-fffr',
@@ -18,4 +36,5 @@ setup(
         'numpy',
         'torch',
     ],
+    cmdclass={'clean': CustomClean},
 )
